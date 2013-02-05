5c6e4a560b7bfb3514f9e50f84d699845016a30f
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2013-02-05 19:45:54 +0000

    added admin UI stuff for assets and added unicode methods to models

--- inventory/admin.py
+++ inventory/admin.py
@@ -4,4 +4,9 @@ from django.contrib import admin
 from inventory.models import Asset, AssetModel
 
 admin.site.register(AssetModel)
-admin.site.register(Asset)
+
+class AssetAdmin(admin.ModelAdmin):
+	list_display = ['tag', 'model']
+	search_fields = ['tag']
+
+admin.site.register(Asset, AssetAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -6,7 +6,12 @@ class AssetModel(models.Model):
 	friendly_name = models.CharField(max_length=255)
 	barcode_name = models.CharField(max_length=255)
 
+	def __unicode__(self):
+		return "%s %s (%s)" % (self.vendor, self.friendly_name, self.barcode_name)
+
 class Asset(models.Model):
 	tag = models.CharField(max_length=10, primary_key=True)
 	model = models.ForeignKey(AssetModel)
 
+	def __unicode__(self):
+		return self.tag
